0c80d10501096d479ccdfc6f1a3da9453b84d075
hanaaman <37300541+hanaaman@users.noreply.github.com>
2018-03-20 22:04:57 +0200

    Add files via upload

---
+++ BUTTON_SEND_CODE.ino
@@ -0,0 +1,62 @@
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
+//RF24 library link https://github.com/maniacbug/RF24
+//Pin Assingmenet http://arduino-info.wikispaces.com/Nrf24L01-2.4GHz-HowTo
+int msg[1];
+int button1 = 4;
+int button2 = 5;
+int button3 = 3-;
+int button4 = 2;
+RF24 radio(9,10);//check your pin number on RF24 github check you have the right
+//pin for the arduino you're using. this pin number is diffrent for diffrent arduino models.
+
+const uint64_t pipe = 0xF0F0F0F0D2L;
+int button1State = 0;
+int button2State = 0;
+int button3State = 0;
+int button4State = 0;
+
+
+void setup(void)
+{
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(pipe);
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+  pinMode(button4, INPUT);
+  
+}
+
+void loop(void)
+{
+  button1State = digitalRead(button1);
+  button2State = digitalRead(button2);
+  button3State = digitalRead(button3);
+  button4State = digitalRead(button4);
+
+  
+  if (button1State == HIGH)//forward
+  {
+    msg[0] = 111;
+    radio.write(msg, 1);
+  }  else if (button2State == HIGH)//back
+  {
+    msg[0] = 222;
+    radio.write(msg, 1);
+  }
+  else if (button3State == HIGH)//right
+  {
+    msg[0] = 333;
+    radio.write(msg, 1);
+  }
+  else if (button4State == HIGH)//left
+  {
+    msg[0] = 444;
+    radio.write(msg, 1);
+  }
+   
+  
+}
---
+++ LED_RECEIVER_CODE.ino
@@ -0,0 +1,90 @@
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>
+int msg[1];
+RF24 radio(9,10);//check your pin number on RF24 github check you have the right
+//pin nu2mber for the arduino you're using. this pin is diffrent for diffrent arduino models.
+
+const uint64_t pipe = 0xF0F0F0F0D2L;
+int mot1 = 5;
+int mot2 = 6;
+int mot3 = 7;
+int mot4 = 8;
+
+
+byte addresses[][6] = {"0"};
+
+void setup(void)
+{
+  pinMode(mot1 , OUTPUT);
+  pinMode(mot2 , OUTPUT);
+  pinMode(mot3 , OUTPUT);
+  pinMode(mot4 , OUTPUT);
+  
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(1, pipe);
+  radio.startListening();
+  
+ 
+}
+
+void loop(void)
+{
+  if(radio.available()){
+    bool done = false;
+    while (!done) {
+      done = radio.read(msg, 1);
+      Serial.println(msg[0]);
+      if (msg[0] == 111) {
+      //delay(10);
+      forward();
+      
+      }
+      else if(msg[0] == 222){
+        back();
+        }
+      else if(msg[0] == 333 ){
+        right();
+        }
+      else if(msg[0] == 444){
+        left();
+        }
+      else{
+        stopcar();
+        }
+  }}}
+  void forward(){
+  digitalWrite(mot1 , HIGH);
+  digitalWrite(mot2 , LOW);
+  digitalWrite(mot3 , HIGH);
+  digitalWrite(mot4 , LOW);
+  
+}
+void back(){
+  digitalWrite(mot1 , LOW);
+  digitalWrite(mot2 , HIGH);
+  digitalWrite(mot3 , LOW);
+  digitalWrite(mot4 , HIGH);
+}
+void right(){
+  digitalWrite(mot1 , HIGH);
+  digitalWrite(mot2 , LOW);
+  digitalWrite(mot3 , LOW);
+  digitalWrite(mot4 , HIGH);
+}
+void left(){
+  digitalWrite(mot1 , LOW);
+  digitalWrite(mot2 , HIGH);
+  digitalWrite(mot3 , HIGH);
+  digitalWrite(mot4 , LOW);
+}
+void stopcar(){
+  digitalWrite(mot1 , LOW);
+  digitalWrite(mot2 , LOW);
+  digitalWrite(mot3 , LOW);
+  digitalWrite(mot4 , LOW);
+}
+
+
